c2f493336a753c629d73e6d7bc4b7c16b72a2984
krskibin <krskibin@gmail.com>
2020-03-23 19:06:57 +0100

    fix: fix errors with types in ayc integration

--- packages/about-you/composables/src/composables/useCategory/index.ts
+++ packages/about-you/composables/src/composables/useCategory/index.ts
@@ -2,7 +2,7 @@ import { getCategory } from '@vue-storefront/about-you-api';
 import { useCategoryFactory } from '@vue-storefront/factories';
 import { UseCategory, BapiCategory } from '../../types';
 
-const useCategory: (id: string) => UseCategory<BapiCategory> = useCategoryFactory<BapiCategory, any>({
+const useCategory: (id: string) => UseCategory<BapiCategory, any> = useCategoryFactory<BapiCategory, any>({
   categorySearch: getCategory
 });
 
