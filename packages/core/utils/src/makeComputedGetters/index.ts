482b51d1269df87b243fd7bed0c81deedeab2e23
krskibin <krskibin@gmail.com>
2020-03-23 14:43:50 +0100

    fix: add missing type

--- packages/core/utils/src/makeComputedGetters/index.ts
+++ packages/core/utils/src/makeComputedGetters/index.ts
@@ -4,7 +4,7 @@ import wrap from '../wrap';
 interface Getters {
   [key: string]: (param: unknown, ...args: any) => Readonly<Ref<Readonly<unknown>>>;
 }
-export default function makeComputedGetters (getters): Getters {
+export default function makeComputedGetters (getters: any): Getters {
   return Object.assign({}, ...Object.keys(getters).map(getterName => {
     return {
       // eslint-disable-next-line no-undef
